b81c6154d7b93ec5de44dd20d09da6d6a29929a4
Iosef Kaver <iosef.kaver@gmail.com>
2014-09-05 19:22:28 -0400

    Changed node id's from ints to Strings

--- hw1-migratable-processes/ds-hw1/src/main/java/com/ikaver/aagarwal/ds/hw1/INodeManager.java
+++ hw1-migratable-processes/ds-hw1/src/main/java/com/ikaver/aagarwal/ds/hw1/INodeManager.java
@@ -3,7 +3,7 @@ package com.ikaver.aagarwal.ds.hw1;
 public interface INodeManager {
   
   public int launch(String className, String [] args);
-  public boolean migrate(int pid, int sourceNode, int destinationNode);
+  public boolean migrate(int pid, String sourceNode, String destinationNode);
   public boolean remove(int pid);
   
 }
