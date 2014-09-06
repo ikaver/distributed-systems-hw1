89f0f034aa13bf82a7f53b65c710efc8e0223481
Iosef Kaver <iosef.kaver@gmail.com>
2014-09-06 18:00:28 -0400

    Added ProcessManagerPool class

---
+++ ds-hw1/src/main/java/com/ikaver/aagarwal/ds/hw1/nodemanager/ProcessManagerPool.java
@@ -0,0 +1,29 @@
+package com.ikaver.aagarwal.ds.hw1.nodemanager;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class ProcessManagerPool {
+
+  private HashMap<String, String> availableConnections;
+  
+  public int size() {
+    return availableConnections.size();
+  }
+  
+  public void add(String id, String connection) {
+    this.availableConnections.put(id, connection);
+  }
+  
+  public String connectionForId(String id) {
+    return this.availableConnections.get(id);
+  }
+  
+  public Set<String> availableNodes() {
+    return this.availableConnections.keySet();
+  }
+  
+  public void remove(String id) {
+    this.availableConnections.remove(id);
+  }
+}
