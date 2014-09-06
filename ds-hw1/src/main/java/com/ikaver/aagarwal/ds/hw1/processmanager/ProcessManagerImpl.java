a0b031edfb2f9ff4998357aa62eb1bb1b5e0d970
Ankit Agarwal <ankit1990@gmail.com>
2014-09-06 16:11:28 -0400

    fixed spurious warnings.

--- ds-hw1/src/main/java/com/ikaver/aagarwal/ds/hw1/processmanager/ProcessManagerImpl.java
+++ ds-hw1/src/main/java/com/ikaver/aagarwal/ds/hw1/processmanager/ProcessManagerImpl.java
@@ -58,22 +58,16 @@ public class ProcessManagerImpl implements IProcessManager {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return true;
