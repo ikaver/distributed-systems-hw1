7104bc1b2dde598f07defc602217a55cebc0ebe5
Iosef Kaver <iosef.kaver@gmail.com>
2014-09-08 14:48:03 -0400

    Added wait time in seconds for refresh definition

--- ds-hw1-shared/src/main/java/com/ikaver/aagarwal/ds/hw1/shared/Definitions.java
+++ ds-hw1-shared/src/main/java/com/ikaver/aagarwal/ds/hw1/shared/Definitions.java
@@ -3,6 +3,6 @@ package com.ikaver.aagarwal.ds.hw1.shared;
 public class Definitions {
   
   public static final String PROCESS_MANAGER_SERVICE = "ProcessManager";
-  
+  public static final int WAIT_TIME_IN_SECONDS_FOR_NODE_MANAGER_REFRESH = 5;
 
 }
