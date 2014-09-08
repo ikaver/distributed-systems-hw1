b44447cadd875544af567ee714b3b89d45676066
Ankit Agarwal <ankit1990@gmail.com>
2014-09-08 16:46:54 -0400

    Configured log manager to print on the console. We can add more configuration options if we needed. Also removed direct dependency of the process manager and node manager.

--- ds-hw1-processmanager/src/main/java/com/ikaver/aagarwal/ds/hw1/processmanager/ProcessManagerEntryPoint.java
+++ ds-hw1-processmanager/src/main/java/com/ikaver/aagarwal/ds/hw1/processmanager/ProcessManagerEntryPoint.java
@@ -6,26 +6,25 @@ import java.rmi.RemoteException;
 
 import com.ikaver.aagarwal.ds.hw1.shared.IProcessManager;
 
-
 public class ProcessManagerEntryPoint {
 
 	public static void main(String args[]) {
-		
-	  IProcessManager manager = null;
-    try {
-      manager = new ProcessManagerImpl();
-    } catch (RemoteException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-    try {
-      Naming.rebind("//localhost:2000/ProcessManager", manager);
-    } catch (RemoteException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+
+		IProcessManager manager = null;
+		try {
+			manager = new ProcessManagerImpl();
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			Naming.rebind("//localhost:2000/ProcessManager", manager);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
