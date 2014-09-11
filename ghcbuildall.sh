ead01fbc42ee17d70c9099a12a29062d02bc5f30
Iosef Kaver <iosef.kaver@gmail.com>
2014-09-11 05:06:35 -0400

    Fixed ghc build script (netbeans path)

--- ghcbuildall.sh
+++ ghcbuildall.sh
@@ -1,11 +1,11 @@
 cd ds-hw1-shared
-/usr/local/lib/netbeans/java/maven/bin/mvn clean install
+/usr/local/netbeans-7.2.1/java/maven/bin/mvn clean install
 cd ..
 cd ds-hw1-processrunner
-/usr/local/lib/netbeans/java/maven/bin/mvn clean
-/usr/local/lib/netbeans/java/maven/bin/mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+/usr/local/netbeans-7.2.1/java/maven/bin/mvn clean
+/usr/local/netbeans-7.2.1/java/maven/bin/mvn assembly:assembly -DdescriptorId=jar-with-dependencies
 cd ..
 cd ds-hw1-nodemanager
-/usr/local/lib/netbeans/java/maven/bin/mvn clean
-/usr/local/lib/netbeans/java/maven/bin/mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+/usr/local/netbeans-7.2.1/java/maven/bin/mvn clean
+/usr/local/netbeans-7.2.1/java/maven/bin/mvn assembly:assembly -DdescriptorId=jar-with-dependencies
 cd ..
